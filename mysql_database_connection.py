433f7470d13b6ac0c8e3b12363b1ba9fc27cca18
DavidDelamater <David.W.Delamater@gmail.com>
2020-06-26 10:44:21 -0500

    Updating common to have the database outline, updating db connection to handle more db jobs

--- mysql_database_connection.py
+++ mysql_database_connection.py
@@ -1,17 +1,23 @@
 import time
 import atexit
 import mysql.connector
+import yaml
+import os
 
 from python_utils import print_msg
 
 class MySQLDatabaseConnection:
-    def __init__(self, address, username, password, database):
+    def __init__(self):
         self.__connection = None
-        self.__address = address
-        self.__username = username
-        self.__password = password
-        self.__database = database
-        self.cursor = None
+        self.__db_config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "database_outline.yaml"))
+        with open(self.__db_config_path) as yaml_file:
+            db_config_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        self.__database = db_config_data['name']
+        self.__address = db_config_data['address']
+        self.__username = db_config_data['username']
+        self.__password = db_config_data['password']
+        self.__known_tables = db_config_data['tables']
+        self.__cursor = None
 
     def open_connection(self, timeout=60):
         print_msg("Opening connection to database...")
@@ -36,31 +42,39 @@ class MySQLDatabaseConnection:
                 print_msg("Failed to connect to database. Trying again...")
                 time.sleep(1)
         print_msg("Connected to database")
-        self.cursor = self.__connection.cursor()
+        self.__cursor = self.__connection.cursor()
         atexit.register(self.close_connection)
 
     def close_connection(self):
         print_msg("Closing connection to database")
         self.__connection.close()
         self.__connection = None
-        self.cursor = None
+        self.__cursor = None
         atexit.unregister(self.close_connection)
 
     def command(self, sql_query):
-        self.cursor.execute(sql_query)
+        self.__cursor.execute(sql_query)
         try:
-            result = self.cursor.fetchall()
+            result = self.__cursor.fetchall()
         except mysql.connector.errors.InterfaceError:
             # The command didn't return any data
             result = None
+        self.__connection.commit()
         return result
 
-    def create_table(self, table_name, column_config_dict):
-        create_table_sql = "CREATE TABLE {0}(".format(table_name)
-        for col_name, col_sql_str in column_config_dict.items():
-            create_table_sql += "{0} {1},".format(col_name, col_sql_str)
-        create_table_sql = create_table_sql[:-1] + ")"
-        self.cursor.execute(create_table_sql)
+    def create_table(self, table):
+        # Check for table in known tables
+        if table in self.__known_tables.keys():
+            # Generate the sql query to create the table
+            table_column_dict = self.__known_tables[table]['columns']
+            create_table_sql = "CREATE TABLE {0}(".format(table_name)
+            for col_name, col_sql_str in table_column_dict.items():
+                create_table_sql += "{0} {1},".format(col_name, col_sql_str)
+            create_table_sql = create_table_sql[:-1] + ")"
+            self.command(create_table_sql)
+        else:
+            print_msg("Table '{0}' not in dict of known tables".format(table))
+            raise Exception
 
     def drop_table(self, table_name):
-        self.cursor.execute("DROP TABLE {0}".format(table_name))
+        self.__cursor.execute("DROP TABLE {0}".format(table_name))
